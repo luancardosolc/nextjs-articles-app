7d3b1fbf6774fecacc37f55839c6a98865ac1a53
Luan Cardoso <luanleitecardoso@gmail.com>
2021-12-29 15:26:49 -0300

    Adding next Head

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,11 @@
+import Head from "next/head";
+
 const about = () => {
   return (
     <div>
+      <Head>
+        <title>NextJS Article App - About</title>
+      </Head>
       <h1>About</h1>
       <p>Cool app</p>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,10 @@ import Head from 'next/head'
 const Home: NextPage = () => {
   return (
     <div>
+      <Head>
+        <title>NextJS Article App</title>
+        <meta name='keyworkds' content='web development, programming, nextjs, javascript' />
+      </Head>
       <h1>Welcome to articles app</h1>
     </div>
   )
