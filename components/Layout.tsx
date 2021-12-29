3818582e9b8a4b36a0d5366430ad3dbeb0e18fe4
Luan Cardoso <luanleitecardoso@gmail.com>
2021-12-29 16:15:24 -0300

    Creating a navbar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,18 +1,22 @@
 import styles from "../styles/Layout.module.css";
 import React, { ReactChildren, ReactChild } from 'react';
+import Nav from "./Nav";
 
 interface AuxProps {
   children: ReactChild | ReactChildren;
 }
 
 const Layout = ({ children }: AuxProps) => {
-	return (
-		<div className={styles.container}>
-			<main className={styles.main}>
-				{children}
-			</main>
-		</div>
-	);
+  return (
+    <>
+      <Nav></Nav>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          {children}
+        </main>
+      </div>
+    </>
+  );
 };
 
 export default Layout;
