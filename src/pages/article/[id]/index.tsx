1f06dba7ec1f497129be85fcc670a463936a6290
Luan Cardoso <luanleitecardoso@gmail.com>
2021-12-29 19:17:49 -0300

    getting the article from the API by the id given by the url

--- src/pages/article/[id]/index.tsx
+++ src/pages/article/[id]/index.tsx
@@ -1,9 +1,11 @@
 import { useRouter } from "next/router"
 
 
-const Article = () => {
+const ArticleComponent = (props: any) => {
+  const { article } = props;
   const router = useRouter();
   const {id} = router.query;
+  console.log('article', article);
   return (
     <div>
       This is an article {id}
@@ -11,4 +13,16 @@ const Article = () => {
   )
 }
 
-export default Article
+export const getServerSideProps = async (context: any) => {
+  const rest = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+  
+  const article = await rest.json();
+
+  return {
+    props: {
+      article
+    }
+  }
+}
+
+export default ArticleComponent
