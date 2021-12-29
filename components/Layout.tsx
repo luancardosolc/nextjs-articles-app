c6435d16292ffc88f7906b5c74cbf0262169b43d
Luan Cardoso <luanleitecardoso@gmail.com>
2021-12-29 16:25:05 -0300

    Article header

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import styles from "../styles/Layout.module.css";
 import React, { ReactChildren, ReactChild } from 'react';
 import Nav from "./Nav";
+import Header from "./Header";
 
 interface AuxProps {
   children: ReactChild | ReactChildren;
@@ -9,9 +10,10 @@ interface AuxProps {
 const Layout = ({ children }: AuxProps) => {
   return (
     <>
-      <Nav></Nav>
+      <Nav />
       <div className={styles.container}>
         <main className={styles.main}>
+          <Header />
           {children}
         </main>
       </div>
